50ecd90d132f3e11ebef1b1f2c94e09300a88d99
yejiuzy <1028615763@qq.com>
2022-09-16 15:33:18 +0800

    分页抓取和客户端缓存

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import './App.css';
 
-// 网络请求
+// 定义常量
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1/';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      result: null,     // 空的列表结果
+      results: null,     // 空的列表结果
+      searchKey: '',
       searchTerm: DEFAULT_QUERY,   // 默认搜索词
     }
 
@@ -22,16 +26,33 @@ class App extends Component {
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
   };
 
   // 将获取到的数据存到result里
   setSearchTopStories(result) {
-    this.setState({result});
+    const {hits, page} = result;
+    const {searchKey, results} = this.state;
+
+    const oldHits = results && results[searchKey]
+                    ? results[searchKey].hits
+                    : [];
+
+    const updateHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: {hits: updateHits, page}
+      }
+    })
   };
 
   // 异步请求数据
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)  // 模板字符串
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)  // 模板字符串
     .then(response => response.json())  // 转化成json格式的数据结构
     .then(result => this.setSearchTopStories(result))  // 处理后的响应赋值给state中的结果
     .catch(e => e);
@@ -45,22 +66,35 @@ class App extends Component {
 
   // onDismiss键忽略项方法
   onDismiss(id) {
+    const {searchKey, results} = this.state;
+    const {hits, page} = results[searchKey];
+
     const isNotId = item => item.objectID !== id;
-    const updateHits = this.state.result.hits.filter(isNotId);
+    const updateHits = hits.filter(isNotId);
     this.setState({
       // result: Object.assign({}, this.state.result, {hits: updateHits})
-      result: {...this.state.result, hits: updateHits}
+      results: {...results, [searchKey]: {hits: updateHits, page}}
     })
   };
 
-  onSearchSubmit() {
+  onSearchSubmit(event) {
     const {searchTerm} = this.state;
-    this.fetchSearchTopStories(searchTerm);
+    this.setState({searchKey: searchTerm});
+
+    if(this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    event.preventDefault();
+  };
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
   };
 
   // 生命周期函数（异步请求数据）
   componentDidMount() {
     const {searchTerm} = this.state;
+    this.setState({searchKey: searchTerm});
     this.fetchSearchTopStories(searchTerm);
   }
 
@@ -68,12 +102,14 @@ class App extends Component {
   // 渲染函数
   render() {
     // console.log(this.state);
-    const {searchTerm, result} = this.state;
-    if(!result) return null;  // 通过返回null来不渲染任何东西
+    const {searchTerm, results, searchKey} = this.state;
+    const page = (results && results[searchKey] && results[searchKey].page) || 0;   // 默认分页为0
+    const list = (results && results[searchKey] && results[searchKey].hits) || [];
+    // if(!) return null;  // 通过返回null来不渲染任何东西
     return (
       <div className='page'>
         <div className='interactions'>
-        <Search 
+        <Search
           value={searchTerm}
           onChange={this.onSearchChange}
           onSubmit = {this.onSearchSubmit}
@@ -81,35 +117,35 @@ class App extends Component {
         Search
         </Search>
         </div>
-        {
-          result
-          ? <Table 
-              list = {result.hits}
-              onDismiss = {this.onDismiss}
-            />
-          : null
-        }
-        
+        <Table
+          list = {list}
+          onDismiss = {this.onDismiss}
+        />
+        <div className='interactions'>
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+          More
+          </Button>
+        </div>
       </div>
     );
   }
 }
 
 // 搜索组件
-const Search = ({value, onChange, onSubmit}) => 
+const Search = ({value, onChange, onSubmit, children}) =>
       <form onSubmit={onSubmit}>
         <input
           type="text"
           value={value}
           onChange={onChange}
         />
-        <button type="button">
-        search
+        <button type="submit">
+        {children}
         </button>
       </form>
 
 // Table组件
-const Table = ({list, onDismiss}) => 
+const Table = ({list, onDismiss}) =>
       <div className='table'>
         {list.map(item =>
           <div key={item.objectID} className='table-row'>
@@ -129,7 +165,7 @@ const Table = ({list, onDismiss}) =>
       </div>
 
 // 按钮组件
-const Button = ({onClick, className = '', children}) => 
+const Button = ({onClick, className = '', children}) =>
       <button
         onClick={onClick}
         className={className}
